f48cc4e8fe096c096a4c242c1320bfa4594ef54f
Victoria <QAInstructorSky@gmail.com>
2025-10-02 19:49:12 +0100

    EDIT: added wip comments

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
 class Calculator:
 
     def add(self, x, y):
+        # return 0
+        # return 2
         return x + y
 
     def subtract(self, x, y):
+        # return 0
         return x - y
